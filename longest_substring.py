e61df2d2cd5ffa3a4eeecbc589d36bab126380dc
Ginger Mei <ccaj007@gmail.com>
2023-03-27 08:37:35 -0400

    update

---
+++ longest_substring.py
@@ -0,0 +1,12 @@
+'''
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+Given a string s, find the length of the longest substring,without repeating characters.
+'''
+from typing import List
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+s = Solution()
+s.lengthOfLongestSubstring()
---
+++ valid_mountain_array.py
@@ -0,0 +1,36 @@
+'''
+https://leetcode.com/problems/valid-mountain-array/
+
+Given an array of integers arr, return true if and only if it is a valid mountain array.
+
+Recall that arr is a mountain array if and only if:
+
+    arr.length >= 3
+    There exists some i with 0 < i < arr.length - 1 such that:
+        arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+        arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+'''
+from typing import List
+
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+
+        if (len(arr) < 3):
+            return False
+
+        i = 1
+
+        while(i < len(arr) and arr[i] > arr[i-1]):
+            i += 1
+
+        if(i == 1 or i == len(arr)):
+            return False
+
+        while( i < len(arr) and arr[i] < arr[i-1]):
+            i += 1
+
+        return i == len(arr)
+
+s = Solution()
+ans = s.validMountainArray([2, 1])
+print(ans)
